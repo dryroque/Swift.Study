0777026306321b1ae12ee6d2000305076cf46171
dryroque <adryannorf@hotmail.com>
2024-01-23 13:48:29 -0300

    Commit
    Colocando o projeto todo no repositorio

---
+++ Swift.Study/apostilas/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Apostila, ViewApostila
+from django.contrib.messages import constants
+from django.contrib import messages
+
+# Create your views here.
+
+# crinado a funcao adicionar apostilas
+def adicionar_apostilas(request):
+    if request.method == 'GET':
+        apostilas = Apostila.objects.filter(user=request.user)
+
+        views_totais = ViewApostila.objects.filter(apostila__user=request.user).count()
+        return render(request, 'adicionar_apostilas.html', {'apostilas': apostilas, 
+                                                            'views_totais': views_totais})
+    elif request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        arquivo = request.FILES['arquivo']
+
+        apostila = Apostila(
+            user=request.user,
+            titulo=titulo,
+            arquivo=arquivo
+        )
+
+        apostila.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Salvo com sucesso')
+
+        return redirect('/apostilas/adicionar_apostilas')
+
+# funcao para ver a apostila
+def apostila(request, id):
+    apostila = Apostila.objects.get(id=id)
+    views_totais = ViewApostila.objects.filter(apostila=apostila).count()
+    views_unicas = ViewApostila.objects.filter(apostila=apostila).values('ip').distinct().count()
+
+    view = ViewApostila(
+        ip=request.META['REMOTE_ADDR'],
+        apostila=apostila
+    )
+    view.save()
+
+    return render(request, 'apostila.html', {'apostila': apostila,
+                                             'views_totais': views_totais,
+                                             'views_unicas': views_unicas})
+
+
+
+
---
+++ Swift.Study/templates/static/flashcard/js/flashcard.js
@@ -0,0 +1,11 @@
+function flip_card(id) {
+    card = document.getElementById(id)
+    console.log(card.style.display)
+            
+        if (card.style.display == 'none' || card.style.display == '') {
+            card.style.display = 'block'
+        } else {
+            card.style.display = 'none'
+        }
+}
+
